ef98a571c88b4a29685ab3d395a125dcbd4bbc9a
Raphael Kong <rkong@h2ad.net>
2017-07-11 17:09:40 +0200

    - Messy code, to be continued

--- server.js
+++ server.js
@@ -17,6 +17,11 @@ var userSocket = {};
 var userSocketId = [];
 var patients = [];
 var config = {};
+var guid = {};
+var currentIndex = 1;
+var trackingId = 0;
+var trackingSocketId = [];
+var trackingGuid = [];
 port=1234;
 app = express();
 app.set('views',__dirname+"/views");
@@ -62,12 +67,6 @@ patientws.on("connection", function(socket){
     if(socketPS) {
         console.log("The socket id of ps is " +socketPS); 
     }
-    /*socket.emit('ps_connected',
-        JSON.stringify(
-            {type:'serverMessage','pssocket': socketPS}
-            )
-        );
-        */
     socket.emit('patient_connected',
         JSON.stringify(
             {type:'serverMessage','socketid': socket.id}
@@ -92,19 +91,71 @@ patientws.on("connection", function(socket){
         );
 */
     socket.on("send_config",function(data){
-        console.log(socket.id+ " has sent its config  guid => " +data.guid+ " orgs => "+data.orgs);
-	
-	//patients.push(JSON.stringify("{guid:" +data.guid+",orgs:" + data.orgs+",socketid:" + socket.id+"}"));
-       // patients.push({'guid':" +data.guid+",'orgs':" + data.orgs+",'socketid':" + socket.id+"}");
-	    config.guid = data.guid;
-	    config.orgs = data.orgs;
-	    config.socketid = socket.id;
-	    patients.push(JSON.stringify(config));
-	    config = {};
+    //    console.log(socket.id+ " has sent its config  guid => " +data.guid+ " orgs => "+data.orgs);
+     //   console.log(socket.patientsocketid+ " has sent its config  guid => " +data.guid+ " orgs => "+data.orgs);
+        var config1 = {};
+        socket.guid = data.guid ;
+        console.log(socket.patientsocketid+ " has sent its config  guid => " +socket.guid+" orgs => "+data.orgs);
+        // tracking guid and other settings
+                config1.guid = data.guid;
+                config1.orgs = data.orgs;
+                config1.socketid = socket.id;
+                //patients.push(JSON.stringify(config));
+                patients.push(config1);
+                currentIndex ++;
+        patients.forEach(function(val,index,array){
+            console.log("Index "+index + " - guid : "+val.guid +" socketid = " + val.socketid);
+        });
+  /*      if(guid[data.guid]) {
+            console.log("Le guid est deja enregistré à la position : " + socket.currentIndex);
+            console.log("Mise à jour de ses parametres");
+            console.log("Ancienne socket ID : " +config.socketid + " => nouvelle : "+socket.id);
+            socket.guid = data.guid;
+	        config.guid = data.guid;
+	        config.orgs = data.orgs;
+	        config.socketid = socket.id;
+            
+            patients[guid.length - guid[guid.data]] = config;
+        }
+        else {
+            console.log("New GUID : " + data.guid);
+            guid[data.guid] = currentIndex;
+            socket.currentIndex = currentIndex;
+            trackingSocketId[socket.id]=currentIndex;
+            trackingGuid[socket.id]=data.guid; 
+            config.guid = data.guid;
+            config.orgs = data.orgs;
+            config.socketid = socket.id;
+            patients.push(JSON.stringify(config));
+            currentIndex ++;
+
+            }
+       
+        console.log("Displaying the current patients connected");
+        patients.forEach(function(val,index,array){
+            var myjson = JSON.parse(array[index]);
+            console.log("Index : " + index + " - Guid : " + myjson.guid +" socket id : "+myjson.socketid);
+        });
+        */
     });
     socket.on("disconnect", function(){
-        console.log("Client disconnected");
-	console.log("The socket id " + socket.id + " is no more available");
+            console.log("Client disconnected");
+            console.log("The socket id " + socket.patientsocketid + " is no more available " + socket.guid);
+            console.log("The socket id " + socket.id + " is no more available");
+            /*console.log("The index of socketid "+ socket.id + " is "+trackingSocketId[socket.id]);
+            console.log("We need to remove the object from the patients array");
+            console.log("Patients.length : "+patients.length);
+            var idToBeDeleted = patients.length-trackingSocketId[socket.id];
+            patients.splice(idToBeDeleted,1);
+            trackingSocketId.splice(idToBeDeleted,1);
+            console.log("GUID to be deleted : " +trackingGuid[socket.id]);
+
+            console.log("Diplay new list : ");
+            patients.forEach(function(val,index,array){
+            var myjson = JSON.parse(array[index]);
+            console.log("Index : " + index + " - Guid : " + myjson.guid +" socket id : "+myjson.socketid);
+        });
+*/
     	io.of('/ps').emit('Client disconnected',
 	JSON.stringify(
 		//{'socket': +socket.id})
--- static/js/ps.js
+++ static/js/ps.js
@@ -1,10 +1,12 @@
 var remote = 'http://sjbw0052.h2ad.local:1234';
-var remote = 'http://localhost:1234';
+//var remote = 'http://localhost:1234';
 var socketNamespace = '/ps';
 var socket = io.connect(remote+socketNamespace);
 
 var roomName = decodeURI(  (RegExp("room" + '=' + '(.+?)(&|$)').exec(location.search)     || [, null])[1]);
 
+
+var guid = [];
 
 socket.on('message',function(data){
     data = JSON.parse(data);
@@ -24,16 +26,9 @@ socket.on('update_list',function(data){
 //    console.log(data);
 	//console.log(data.length);
 	data.forEach(function(val,index,array){
-	var myjson = JSON.parse(array[index]);
-		//console.log("GUID : " + myjson.guid);
+	//var myjson = JSON.parse(array[index]);
+		console.log("GUID : " + val.guid);
 	});
-	for (i=0;i<data.length;i++){
-		var myjson = JSON.parse(data[i]);
-		//console.log(data[i]);
-		console.log(myjson.socketid);
-		console.log(myjson.guid);
-		console.log(myjson.orgs);
-	}
 
 });
 
@@ -43,18 +38,63 @@ socket.on("Client disconnected",function(data){
 });
 
 $(document).ready(function(){
-//$(function(){
-    $('#send').click(function(){
-        var data = {
-            message: $('#message').val(),
-            type: 'userMessage',
-            name: $('#nickname').val(),
+    $('#messages').html=''; 
+    var test = [];
+    test['azerty']="azeerty";
+    test['qwerty']="qwerrty";
+    console.log("Testing test object :");
+    console.log(test['azerty']);
+    console.log(test.azerty);
+    console.log(delete test.azerty);
+    console.log("After delete " +test.azerty);
+    console.log("=================================");
+    guid.push("val1");
+    guid.push("val2");
+    for (i=0;i<guid.length;i++){
+        console.log("Index "+ i +" : "+ guid[i]);
+    }
+    guid.forEach(function(val,index,array){
+        console.log("current val :"+val +" current index : "+index+" current array : " +array);
+        if(val === "val1"){
+            array.pop(index);
         }
-        socket.send(JSON.stringify(data));
-        $('#message').val('');
     });
-    $('#setname').click(function(){
-        socketsystem.emit('set_name',{name : $('#nickname').val()});
-        console.log("Emitting set_name event : " + $('#nickname').val());
+    var inventaire = [
+        {guid: '123456', socketid: 'aarrgGG', orgs: [0,1]},
+        {guid: '112233', socketid: 'AAZEERs', orgs: [3,1]},
+        {guid: '998876', socketid: 'ppOLLKq', orgs: [12,19]},
+        
+    ];
+    var data = {} ;
+    inventaire.forEach(function(val, index,array){
+        console.log("Index "+index + " - guid : "+val.guid +" socketid = " + val.socketid);
+    });
+    console.log("Updating one element : guid=123456 => socket.id would be set to aaaaaa");
+    inventaire[1].socketid='aaaaaaa';
+    inventaire.forEach(function(val, index,array){
+        console.log("Index "+index + " - guid : "+val.guid +" socketid = " + val.socketid);
+    });
+
+    console.log("Try to update an object of array");
+    data.guid = '000000';
+    data.socketid = '000000';
+
+    inventaire[1] = data;
+    inventaire.forEach(function(val, index,array){
+        console.log("Index "+index + " - guid : "+val.guid +" socketid = " + val.socketid);
+    });
+
+    inventaire.push({guid: '999999',socketid: 'ssDDFFff'});
+    inventaire.forEach(function(val, index,array){
+        console.log("Index "+index + " - guid : "+val.guid);
+    });
+//    inventaire.splice(1,1);
+    inventaire.forEach(function(val, index,array){
+        console.log("Index "+index + " - guid : "+val.guid);
+        $('#connected').append('<div id="'+val.guid+'">'+val.guid + ' (socket : '+val.socketid+')</div>');
+        $('#'+val.guid).click(function(){
+            $('#'+val.guid).slideUp();
+        });
     });
 });
+
